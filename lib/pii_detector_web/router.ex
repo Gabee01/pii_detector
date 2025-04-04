4159f3e4098fd3dc06faa440bf476f05f7560ab9
Gabriel Carraro <gacarraro@gmail.com>
2025-04-04 12:59:03 -0300

    removes user routes and ui

--- lib/pii_detector_web/router.ex
+++ lib/pii_detector_web/router.ex
@@ -1,8 +1,6 @@
 defmodule PiiDetectorWeb.Router do
   use PiiDetectorWeb, :router
 
-  import PiiDetectorWeb.UserAuth
-
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -10,7 +8,6 @@ defmodule PiiDetectorWeb.Router do
     plug :put_root_layout, html: {PiiDetectorWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -29,43 +26,5 @@ defmodule PiiDetectorWeb.Router do
 
     post "/webhooks/slack", WebhookController, :slack
     post "/webhooks/notion", WebhookController, :notion
-  end
-
-  ## Authentication routes
-
-  scope "/", PiiDetectorWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
-
-    live_session :redirect_if_user_is_authenticated,
-      on_mount: [{PiiDetectorWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
-      live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
-    end
-
-    post "/users/log_in", UserSessionController, :create
-  end
-
-  scope "/", PiiDetectorWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    live_session :require_authenticated_user,
-      on_mount: [{PiiDetectorWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
-    end
-  end
-
-  scope "/", PiiDetectorWeb do
-    pipe_through [:browser]
-
-    delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{PiiDetectorWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
-    end
   end
 end
