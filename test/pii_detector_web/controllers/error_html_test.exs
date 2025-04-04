e5ec7ac4b99ddf4994be438720d50c89de707c53
Gabriel Carraro <gacarraro@gmail.com>
2025-04-04 12:54:35 -0300

    format

--- test/pii_detector_web/controllers/error_html_test.exs
+++ test/pii_detector_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule PiiDetectorWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(PiiDetectorWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(PiiDetectorWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
