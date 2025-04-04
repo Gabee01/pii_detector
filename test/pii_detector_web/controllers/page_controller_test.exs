80d675e8b36773a135a4b188eab4ef9b4714445d
Gabriel Carraro <gacarraro@gmail.com>
2025-04-04 12:57:42 -0300

    removes accounts

--- test/pii_detector_web/controllers/page_controller_test.exs
+++ test/pii_detector_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule PiiDetectorWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Get rid of PII in your data"
   end
 end
