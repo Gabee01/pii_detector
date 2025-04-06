c8a0edb6ce13b7e9592403d2970e0391dd83b782
Gabriel Carraro <gacarraro@gmail.com>
2025-04-06 00:45:27 -0300

    tests and format

--- lib/pii_detector_web/controllers/page_controller.ex
+++ lib/pii_detector_web/controllers/page_controller.ex
@@ -6,7 +6,8 @@ defmodule PIIDetectorWeb.PageController do
     # so skip the default app layout.
     render(conn, :home,
       layout: false,
-      slack_invite: "https://join.slack.com/t/jumpchallenge/shared_invite/zt-32y4fno6v-qJQ6rKU0qMO7yYa44FlR0w",
+      slack_invite:
+        "https://join.slack.com/t/jumpchallenge/shared_invite/zt-32y4fno6v-qJQ6rKU0qMO7yYa44FlR0w",
       notion_url: "https://www.notion.so/JUMP-PII-PROD-1cd30e6a7e4c8020b789f0dfa6285117?pvs=4"
     )
   end
